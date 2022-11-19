b90629f44d97871a2d30185b80a33404c49727d3
AguilarRaul <93731101+AguilarRaul@users.noreply.github.com>
2022-11-18 20:37:50 -0800

    Added optional positional argument

--- src/demo.py
+++ src/demo.py
@@ -5,9 +5,10 @@
 Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
-<arg1>             Takes any value (this is a required positional argument)
+<arg1>            Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[<arg4>]          Takes any value (this is an optional positional argument)
 """ 
 
 from docopt import docopt
